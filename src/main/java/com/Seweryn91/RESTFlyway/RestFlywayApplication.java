043277eb9b06d6c03b74d2c0e3cbfc180b20e4da
Seweryn91 <s.talaj@gmail.com>
2020-04-19 18:37:05 +0200

    Initial commit

---
+++ src/main/java/com/Seweryn91/RESTFlyway/RestFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.Seweryn91.RESTFlyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFlywayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Seweryn91/RESTFlyway/RestFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Seweryn91.RESTFlyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
