66b75bea1d801b2b29e6a38ccbc643bcb1243f1b
Seweryn91 <s.talaj@gmail.com>
2020-04-23 17:54:01 +0200

    Create DML file

---
+++ src/main/resources/db/migration/V2__data.sql
@@ -0,0 +1,7 @@
+INSERT INTO customers (id, first_name, last_name, email, phone, country, city, zipcode, street_number) VALUES
+(DEFAULT, 'Aegon II', 'Targaryen', 'jonsnuuh@iknownothi.ng', '33443344', 'Seven Kingdoms', 'Kings Landing', '00-001', 'Red Keep 1/1'),
+(DEFAULT, 'John', 'Doe', 'john@fakemail.com', '1234567890', 'Nevercountry', 'Nevercity', '99-999', 'Nowhere 0/0'),
+(DEFAULT, 'Tom', 'Collins', 'youhavenopants@hoax.com', '00112233', 'United States of America', 'New York', 'NY 10021', 'East 64th St.'),
+(DEFAULT, 'Tommy', 'Vercetti', 'gogetsomesleep@vice.com', '01399323', 'United States of America', 'Vice City', 'GVC 0323', '32 Kaufman St.'),
+(DEFAULT, 'Vespa', 'Vespula', 'yellowjacket@buzz.com', '99999999', 'Nestovia', 'Nestville', 'NV1337', '11 Bees Blvd.'),
+(DEFAULT, 'Arthur', 'Shelby', 'arthur@peakyblinders.inc', '123094593', 'Great Britain', 'Birmingham, England', 'B109QP', '564 Bertram Rd');
